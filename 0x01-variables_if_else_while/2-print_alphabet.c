935cafff491c0a1382157b008de38ecb5ee51177
AshTechs <ashbelh@gmail.com>
2023-09-08 14:34:44 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,17 +3,16 @@
 #include <stdio.h>
 /**
  * main - Prints the alphabet in lowercase
- * Return 0
+ * Return: 0
  */
 int main(void)
 {
 	int lowerCase = 'a';
-
-	while (lowerCase <= 'z')
+while (lowerCase <= 'z')
 	{
 		putchar(lowerCase);
 		lowerCase += 1;
 	}
 	putchar('\n');
-	return (0);
+return (0);
 }
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+int num;
+while (num < 10)
+{
+printf("%d", num);
+num += 1;
+}
+printf("\n");
+return (0);
+}
