f14333072145482718724693ff23d10c4f3f9821
AshTechs <ashbelh@gmail.com>
2023-09-08 19:13:47 -0700

    Final

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,7 +7,7 @@ int main(void)
 {
 int i = '0';
 int j = '0';
-while (i <= '9')
+while (i <= '8')
 {
 while (j <= '9')
 {
@@ -15,7 +15,7 @@ if (!(i > j) || i == j)
 {
 putchar(i);
 putchar(j);
-if (i == '9' && j == '9')
+if (i == '8' && j == '9')
 {
 putchar('\n');
 }
