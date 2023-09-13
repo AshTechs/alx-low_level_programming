78e027b979afc9c4eb5ede8629028d633ce4b93d
AshTechs <ashbelh@gmail.com>
2023-09-13 14:45:27 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <studio.h>
+/**
+ * main - Print the sum
+ * Return: 0a
+ */
+int main(void)
+
+{
+	int a, b = 0;
+
+	while (a < 1024)
+	{
+	if ((a % 3 == 0) || (a % 5 == 0))
+	{
+	b += a;
+	}
+	a++;
+	}
+	printf("%d\n", b);
+	return (0);
+}
