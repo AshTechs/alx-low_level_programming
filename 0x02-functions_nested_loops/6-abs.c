21ac2c3714f92a3ae0db40b007e9df524a9516d8
AshTechs <ashbelh@gmail.com>
2023-09-12 19:56:57 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - Absolute integer
+ * @n: The num
+ * Return: Absolte or 0
+ */
+int _abs(int c)
+{
+if (c < 0)
+{
+int abs_val;
+abs_val = c * -1;
+return (abs_val);
+}
+return (c);
+}
