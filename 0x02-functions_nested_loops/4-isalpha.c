799fbad00ea4a97faae0d28d3241661d4f5a77e9
AshTechs <ashbelh@gmail.com>
2023-09-12 19:22:37 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - Alphabets 
+ * @c: X'ter to b chkd
+ * Return: 1 or 2
+ */
+int _isalpha(int c)
+{
+if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+{
+return (1);
+}
+return (0);
+}
