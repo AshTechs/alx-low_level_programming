97d8205a7a3c52104a260757367eae6115e01401
AshTechs <ashbelh@gmail.com>
2023-09-14 17:52:38 -0700

    3-main_a.c

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -9,19 +9,21 @@
 
 int main(void)
 {
-    int month;
-    int day;
-    int year;
+int month;
+int day;
+int year;
 
-    month = 4;
-    day = 01;
-    year = 1997;
+month = 4;
 
-    printf("Date: %02d/%02d/%04d\n", month, day, year);
+day = 01;
 
-    day = convert_day(month, day);
+year = 1997;
 
-    print_remaining_days(month, day, year);
+printf("Date: %02d/%02d/%04d\n", month, day, year);
 
-    return (0);
+day = convert_day(month, day);
+
+print_remaining_days(month, day, year);
+
+return (0);
 }
