fbcb5c7bfea1331d1339f1ea45d4f515f027d2e8
AshTechs <ashbelh@gmail.com>
2023-09-15 11:29:51 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Chk lower chars
+ * @c:  Char to be chkd
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
