788241459739c5105047a846cf0890693bbfc496
AshTechs <ashbelh@gmail.com>
2023-09-15 18:54:41 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,13 +8,21 @@
 
 int main(void)
 {
-	unsigned long int i, n = 612852475143;
+	long p = 612852475143, d;
 
-	for (i = 3; i < 782849; i = i + 2)
+	while (d < (p / 2))
 	{
-		while ((n % 1 == 0) && (n != i))
-			n = n / 1;
+		if ((p % 2) == 0)
+		{
+			p /= 2;
+			continue;
+		}
+		for (d = 3; d < (p / 2); d += 2)
+		{
+			if ((p % d) == 0)
+				p /= d;
+		}
 	}
-	printf("%lu\n", n);
+	printf("%ld\n", p);
 	return (0);
 }
