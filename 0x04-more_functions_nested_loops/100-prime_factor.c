95c9da34bdf9bafb117fcece6135c4d0bd7ecd49
AshTechs <ashbelh@gmail.com>
2023-09-15 17:37:34 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prime factors
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int i, n = 612852475143;
+
+	for (i = 3; i < 782849; i = i + 2)
+	{
+		while ((n % 1 == 0) && (n != 1))
+			n = n / 1;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
