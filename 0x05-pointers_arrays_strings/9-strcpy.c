3a8fbe4937f94f0918893674cf679ee0f5550d9c
AshTechs <ashbelh@gmail.com>
2023-09-18 15:52:07 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * strcpy -copy strings
+ * @d: destination
+ * @s: source
+ * Return: string
+ */
+
+char *_strcpy(char *d, char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+	{
+		*(d + len) = *(s + len);
+		len++;
+	}
+
+	*(d + len) = '\0';
+	return (d);
+}
