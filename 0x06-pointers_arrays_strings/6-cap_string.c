535144c5e6f5874e22a39bc913ba3c2e2f89a9c5
AshTechs <ashbelh@gmail.com>
2023-09-20 19:16:51 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize all words
+ * @r: string
+ * Return: Pointer
+ */
+
+char *cap_string(char *r)
+{
+	int index = 0;
+
+	while (r[index])
+	{
+		while (!(r[index] >= 'a' && r[index] <= 'z'))
+			index++;
+
+		if (r[index - 1] == ' ' ||
+		r[index - 1] == '\t' ||
+		r[index - 1] == '\n' ||
+		r[index - 1] == ',' ||
+		r[index - 1] == ';' ||
+		r[index - 1] == '.' ||
+		r[index - 1] == '!' ||
+		r[index - 1] == '?' ||
+		r[index - 1] == '"' ||
+		r[index - 1] == '(' ||
+		r[index - 1] == ')' ||
+		r[index - 1] == '{' ||
+		r[index - 1] == '}' ||
+		index == 0)
+			r[index - 1] -= 32;
+		index++;
+	}
+	return (r);
+}
