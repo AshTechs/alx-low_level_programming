a9fe46debf6c61bd9b8dcff13c98df26d684d424
AshTechs <ashbelh@gmail.com>
2023-10-05 15:49:36 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,16 +31,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		for (i = 0; s1[i] != '\0'; i++)
 			o[i] = s1[i];
 		for (i = 0; s1[i] != '\0'; i++)
-			o[s1len + 1] = s2[i];
-		o[s1len + 1] = '\0';
+			o[s1len + i] = s2[i];
+		o[s1len + i] = '\0';
 	}
 	else
 	{
 		for (i = 0; s1[i] != '\0'; i++)
 			o[i] = s1[i];
 		for (i = 0; i < n; i++)
-			o[s1len + 1] = s2[i];
-		o[s1len + 1] = '\0';
+			o[s1len + i] = s2[i];
+		o[s1len + i] = '\0';
 	}
 	return (o);
 }
