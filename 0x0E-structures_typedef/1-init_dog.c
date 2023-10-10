0813e0384006cc7e26bae703950fe930bc0b4bde
AshTechs <ashbelh@gmail.com>
2023-10-09 22:11:16 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "dog.h"
+#include <string.h> // For strcpy
 
 /**
  * init_dog - init a variable
@@ -12,8 +13,17 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	d->name = name;
+		return; /**Handle the case of a NULL pointer gracefully*/
+
+    /*Allocate memory for name and owner strings and copy the data*/
+	d->name = malloc(strlen(name) + 1); /*+1 for the null terminator
+    if*/
+	(d->name != NULL)
+		strcpy(d->name, name);
+
 	d->age = age;
-	d->owner = owner;
+
+	d->owner = malloc(strlen(owner) + 1); /** +1 for the null terminator*/
+	if (d->owner != NULL)
+		strcpy(d->owner, owner);
 }
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include "dog.h"
 
-
 /**
  * free_dog - frees memory allocated for a struct dog
  * @d: struct dog to free
@@ -11,8 +10,16 @@ void free_dog(dog_t *d)
 {
 	if (d)
 	{
-		free(d->name);
-		free(d->owner);
+		/** 
+		 * Free the dynamically allocated name and owner strings
+		 */
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+        /**
+	 * Free the struct dog itself
+	 */
 		free(d);
 	}
 }
