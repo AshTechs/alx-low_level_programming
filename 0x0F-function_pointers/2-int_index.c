9eced02b5b917471eafead8c647db7899c70e2c6
AshTechs <ashbelh@gmail.com>
2023-10-11 07:58:49 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "functions_pointers.h"
+#include "stdio.h"
+
+/**
+ * int_index - Return index place
+ * @a: Array
+ * @s: Size of elements
+ * @p: Pointer to the function
+ * Return: void
+ */
+
+int int_index(int *a, int s, int (*p)(int))
+{
+	int i;
+
+	if (a == NULL || s <= 0 || p == NULL)
+		return (-1);
+	for (i = 0; i < s; i++)
+	{
+		if (p(a[i]))
+			return (i);
+	}
+	return (-1);
+}
