c1e28f68f57ddafb4269a4017fdf2e4025c74656
AshTechs <ashbelh@gmail.com>
2023-10-31 05:11:03 -0700

    Update 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,12 +2,13 @@
 #include <stdio.h>
 
 /**
- * error_file - checks if files can be opened.
- * @file_from: file_from.
- * @file_to: file_to.
- * @argv: arguments vector.
- * Return: no return.
+ * error_file - Checks if files can be opened.
+ * @file_from: File_from.
+ * @file_to: File_to.
+ * @argv: Arguments vector.
+ * Return: Void.
  */
+
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
@@ -23,11 +24,12 @@ void error_file(int file_from, int file_to, char *argv[])
 }
 
 /**
- * main - check the code for Holberton School students.
- * @argc: number of arguments.
- * @argv: arguments vector.
+ * main - Check the code for Holberton School students.
+ * @argc: Number of arguments.
+ * @argv: Arguments vector.
  * Return: Always 0.
  */
+
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
