92354171d41cbe30321a419b0fffaa59b78b7b34
AshTechs <ashbelh@gmail.com>
2023-12-07 18:36:48 -0800

    6-sum_dlistint.c

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Sums all the data of a dlistint_t list
+ * @h: Head of the dlistint_t list
+ * Return: The sum of all the data
+ */
+
+int sum_dlistint(dlistint_t *h)
+{
+	int sum = 0;
+
+	while (h)
+	{
+		sum += h->n;
+		h = h->next;
+	}
+
+	return (sum);
+}
