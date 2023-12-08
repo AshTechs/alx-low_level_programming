c32399eeb65baeaab864cd42d983b04a5ab2f789
AshTechs <ashbelh@gmail.com>
2023-12-07 18:24:28 -0800

    5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Locate a node in a dlistint_t list
+ * @h: Head of the dlistint_t list
+ * @index: The node to locate
+ * Return: Head if successful, NULL if not
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *h, unsigned int index)
+{
+	for (; index != 0; index--)
+	{
+		if (h == NULL)
+			return (NULL);
+		h = h->next;
+	}
+	return (head);
+}
